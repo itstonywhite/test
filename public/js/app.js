b1cbd0421f7d76cd750aaef6c2085a168e815a43
Tony <itsthetonywhite@gmail.com>
2026-08-22 21:37:15 +0330

    Feature: Toggle Theme Btn

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,27 @@
 const $ = document;
 const toggleThemeBtn = $.querySelector("#toggleTheme");
+const themeIcon = $.querySelector("#theme-icon");
+
+window.addEventListener("load", () => {
+  const savedTheme = localStorage.getItem("theme");
+  document.documentElement.className = savedTheme || "";
+  if (savedTheme === "dark") {
+    themeIcon.setAttribute("href", "#sun");
+  } else {
+    themeIcon.setAttribute("href", "#moon");
+  }
+
+  toggleThemeBtn.addEventListener("click", () => {
+    const currentTheme = localStorage.getItem("theme");
+
+    if (currentTheme === "dark") {
+      document.documentElement.className = "";
+      localStorage.setItem("theme", "light");
+      themeIcon.setAttribute("href", "#moon");
+    } else {
+      document.documentElement.className = "dark";
+      localStorage.setItem("theme", "dark");
+      themeIcon.setAttribute("href", "#sun");
+    }
+  });
+});
